2f86461511c1463c8b4bd15409267a42a1fcb25d
prehensile <me@prehensile.co.uk>
2018-08-28 22:39:59 +0100

    added some docs

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-An SMS responder bot
+# An SMS responder bot
 
 Currently has backends for Nexmo, Jasmin coming soon.
+
+## Overview
+`main.py` contains a Flask web service which implements a 'webhook' for SMS delivery services, such as Nexmo.
+Incoming messages are passed to the bot modules in `/bots` to generate replies.
+
+Have a look at [`bots/basebot.py`](bots/basebot.py) for a skeleton bot class with explanatory comments, and [`bots/storiesbot/py`](bots/storiesbot/py) for an example working bot.
--- app/bots/basebot.py
+++ app/bots/basebot.py
@@ -1,3 +1,28 @@
 class BaseBot( object ):
+
+    """
+    Base class for bots.
+    Extend and override these empty functions to create a workig bot.
+    See storiesbot.py for an example working bot.
+    """
+
     def __init__( self ):
         pass
+    
+
+    def can_handle( self, message ):
+        """
+        Should return True or False depending on whether this bot can reply to the passed message. 
+        Something like:
+            return message.lower().startswith("trigger_word")
+        Would give you a bot which replies to messages starting with "trigger_word".
+        """
+        return False
+
+
+    def handle( self, message ):
+        """
+        Handle (process) a message.
+        Should return a reply to that message, which gets sent back to the originating mobile number.
+        """
+        return "Hello, world!"
